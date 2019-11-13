9e92ec3dc3fb2104384b24055e9bf1185a97fc7a
Tanguy <blondiaux.tanguy@gmail.com>
2019-11-13 21:46:36 +0100

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:      'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '57A Shoreditch High St, London E1 6PQ',
+    category:        "belgian"
+  },
+  {
+    name:         'Chez Paul',
+    address:      '53A Shoreditch High St, London E1 6PQ',
+    category:        "japanese"
+  },
+  {
+    name:         'Tacos paradise',
+    address:      '1 Boundary St, London E2 7JE',
+    category:        "italian"
+  },
+  {
+    name:         'Chez Juju',
+    address:      '2 Boundary St, London E2 7JE',
+    category:        "french"
+  }
+]
+Restaurant.create!(restaurants_attributes)
